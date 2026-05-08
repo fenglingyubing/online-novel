8d4163963ea96df9c0464334bd0bac231b3c06bf
fenglingyubing <3284602218@qq.com>
2026-05-08 15:40:18 +0800

    项目搭建

---
+++ src/main/java/com/fengling/OnlineNovelApplication.java
@@ -0,0 +1,13 @@
+package com.fengling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineNovelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengling/OnlineNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineNovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
