c9985c807c217245d97eaa86ab02b75b1b7dbe78
fenglingyubing <3284602218@qq.com>
2026-05-20 10:48:43 +0800

    feat: 增加了三个章节表字段

--- src/main/java/com/fengling/entity/ChapterInfo.java
+++ src/main/java/com/fengling/entity/ChapterInfo.java
@@ -12,42 +12,67 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @TableName("book_chapter")
 public class ChapterInfo {
+
     /**
      * 章节id
      */
     private Long id;
+
     /**
      * 小说id
      */
     private Long bookId;
+
     /**
      * 章节序号
      */
     private Integer chapterNum;
+
     /**
      * 章节名称
      */
     private String chapterName;
+
     /**
      * 章节正文
      */
     private String chapterContent;
+
     /**
      * 章节字数
      */
     private Integer wordCount;
+
     /**
      * 章节状态（0-草稿，1-已发布，2-下架，3-审核中）
      */
     private Integer chapterStatus;
+
     /**
      * 发布时间
      */
     private LocalDateTime publishTime;
+
+    /**
+     * 审核备注
+     */
+    private String auditRemark;
+
+    /**
+     * 审核时间
+     */
+    private String auditTime;
+
+    /**
+     * 审核人id
+     */
+    private Long auditId;
+
     /**
      * 创建时间
      */
     private LocalDateTime createTime;
+
     /**
      * 修改时间
      */
